8a53c506865c31627bd58b7a1e4dca61e4e06827
Shreya S <shreyareddy3002@gmail.com>
2021-11-25 19:06:34 +0530

    first commmit

---
+++ diabetes GradientBoostingClassifier.py
@@ -0,0 +1,21 @@
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
+diabetes = pd.read_csv('diabetes.csv')
+
+X_train, X_test, y_train, y_test = train_test_split(diabetes.loc[:, diabetes.columns != 'Outcome'], diabetes['Outcome'], stratify=diabetes['Outcome'], random_state=66)
+
+from sklearn.ensemble import GradientBoostingClassifier
+gb = GradientBoostingClassifier(random_state=0)
+gb.fit(X_train, y_train)
+print("Accuracy on training set: {:.3f}".format(gb.score(X_train, y_train)))
+print("Accuracy on test set: {:.3f}".format(gb.score(X_test, y_test)))
+
+gb1 = GradientBoostingClassifier(random_state=0, max_depth=1)
+gb1.fit(X_train, y_train)
+print("Accuracy on training set: {:.3f}".format(gb1.score(X_train, y_train)))
+print("Accuracy on test set: {:.3f}".format(gb1.score(X_test, y_test)))
+gb2 = GradientBoostingClassifier(random_state=0, learning_rate=0.01)
+gb2.fit(X_train, y_train)
+print("Accuracy on training set: {:.3f}".format(gb2.score(X_train, y_train)))
+print("Accuracy on test set: {:.3f}".format(gb2.score(X_test, y_test)))
